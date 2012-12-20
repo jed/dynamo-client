1db530b3540f4b3445672eca5f694b21aee02c67
Michael Hart <michael.hart.au@gmail.com>
2012-12-20 19:51:35 +1100

    Consolidate Signature into Request

--- dynamo_client.js
+++ dynamo_client.js
@@ -4,16 +4,16 @@ var https  = require("https")
 var crypto = require("crypto")
 
 function Database(host, credentials) {
-  this.host      = host
-  this.signature = new Signature(credentials)
+  this.host        = host
+  this.credentials = new Credentials(credentials || {})
 }
 
 Database.prototype.request = function(target, data, cb) {
   !function retry(database, i) {
     var req = new Request(database.host, target, data || {})
 
-    database.signature.sign(req)
-    
+    req.sign(database.credentials)
+
     req.send(function(err, data) {
       if (err) {
         if (i < Request.prototype.maxRetries && (
@@ -53,6 +53,14 @@ Request.prototype.target      = "DynamoDB_20111205."
 Request.prototype.service     = "dynamodb"
 Request.prototype.maxRetries  = 10
 Request.prototype.contentType = "application/x-amz-json-1.0"
+
+Request.prototype.sign = function(credentials, date) {
+  var datetime = (date || new Date).toISOString().replace(/[:\-]|\.\d{3}/g, "")
+    , signer = new RequestSigner(credentials, this, datetime)
+
+  this.headers["Date"] = this.headers["X-Amz-Date"] = datetime
+  this.headers["Authorization"] = signer.createAuthHeader()
+}
 
 Request.prototype.send = function(cb) {
   var request = http.request(this, function(res) {
@@ -96,28 +104,16 @@ function Credentials(attrs) {
   }
 }
 
-function Signature(attrs) {
-  this.credentials = new Credentials(attrs || {})
-}
-
-Signature.prototype.sign = function(request, date) {
-  var datetime = (date || new Date).toISOString().replace(/[:\-]|\.\d{3}/g, "")
-    , authRequest = new SignatureRequest(this.credentials, request, datetime)
-
-  request.headers["Date"] = request.headers["X-Amz-Date"] = datetime
-  request.headers["Authorization"] = authRequest.createHeader()
-}
-
 // credentials expects: { accessKeyId, secretAccessKey }
 // request expects: { method, pathname, headers, body, region, service }
 // datetime expects: "yyyymmddTHHMMSSZ"
-function SignatureRequest(credentials, request, datetime) {
+function RequestSigner(credentials, request, datetime) {
   this.credentials = credentials
   this.request = request
   this.datetime = datetime
 }
 
-SignatureRequest.prototype.createHeader = function() {
+RequestSigner.prototype.createAuthHeader = function() {
   return [
     "AWS4-HMAC-SHA256 Credential=" + this.credentials.accessKeyId + "/" + this.credentialString(),
     "SignedHeaders=" + this.signedHeaders(),
@@ -125,7 +121,7 @@ SignatureRequest.prototype.createHeader = function() {
   ].join(", ")
 }
 
-SignatureRequest.prototype.signature = function() {
+RequestSigner.prototype.signature = function() {
   var kDate = this.sha256Digest("AWS4" + this.credentials.secretAccessKey, this.datetime.substr(0, 8))
   var kRegion = this.sha256Digest(kDate, this.request.region)
   var kService = this.sha256Digest(kRegion, this.request.service)
@@ -133,7 +129,7 @@ SignatureRequest.prototype.signature = function() {
   return this.sha256Digest(kCredentials, this.stringToSign(), "hex")
 }
 
-SignatureRequest.prototype.stringToSign = function() {
+RequestSigner.prototype.stringToSign = function() {
   return [
     "AWS4-HMAC-SHA256",
     this.datetime,
@@ -142,7 +138,7 @@ SignatureRequest.prototype.stringToSign = function() {
   ].join("\n")
 }
 
-SignatureRequest.prototype.canonicalString = function() {
+RequestSigner.prototype.canonicalString = function() {
   var pathSplit = this.request.pathname.split("?", 2)
   return [
     this.request.method,
@@ -154,7 +150,7 @@ SignatureRequest.prototype.canonicalString = function() {
   ].join("\n")
 }
 
-SignatureRequest.prototype.canonicalHeaders = function() {
+RequestSigner.prototype.canonicalHeaders = function() {
   var sig = this
   return Object.keys(this.request.headers)
     .sort(function(a, b) { return a.toLowerCase() < b.toLowerCase() ? -1 : 1 })
@@ -162,18 +158,18 @@ SignatureRequest.prototype.canonicalHeaders = function() {
     .join("\n")
 }
 
-SignatureRequest.prototype.canonicalHeaderValue = function(key) {
-  return this.request.headers[key].toString().replace(/\s+/g, " ").replace(/^\s+|\s+$/g, "")
+RequestSigner.prototype.canonicalHeaderValue = function(key) {
+  return this.request.headers[key].toString().replace(/\s+/g, " ").trim()
 }
 
-SignatureRequest.prototype.signedHeaders = function() {
+RequestSigner.prototype.signedHeaders = function() {
   return Object.keys(this.request.headers)
     .map(function(key) { return key.toLowerCase() })
     .sort()
     .join(";")
 }
 
-SignatureRequest.prototype.credentialString = function() {
+RequestSigner.prototype.credentialString = function() {
   return [
     this.datetime.substr(0, 8),
     this.request.region,
@@ -182,15 +178,14 @@ SignatureRequest.prototype.credentialString = function() {
   ].join("/")
 }
 
-SignatureRequest.prototype.sha256Digest = function(key, data, encoding) {
+RequestSigner.prototype.sha256Digest = function(key, data, encoding) {
   return crypto.createHmac("sha256", key).update(data).digest(encoding)
 }
 
-dynamo.Database         = Database
-dynamo.Request          = Request
-dynamo.Credentials      = Credentials
-dynamo.Signature        = Signature
-dynamo.SignatureRequest = SignatureRequest
+dynamo.Database      = Database
+dynamo.Request       = Request
+dynamo.Credentials   = Credentials
+dynamo.RequestSigner = RequestSigner
 
 dynamo.createClient = function(host, credentials) {
   return new Database(host, credentials)
