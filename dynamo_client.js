723bc00ef34dae15be5cf784ebd357dd3d2224ef
Michael Hart <michael.hart.au@gmail.com>
2012-12-20 19:34:18 +1100

    Remove Session strategy altogether

--- dynamo_client.js
+++ dynamo_client.js
@@ -3,42 +3,37 @@ var http   = require("http")
 var https  = require("https")
 var crypto = require("crypto")
 
-function Database(host, strategy) {
-  if (!strategy || typeof strategy.sign !== 'function') {
-    strategy = new Signature(strategy)
-  }
-  this.host     = host
-  this.strategy = strategy
+function Database(host, credentials) {
+  this.host      = host
+  this.signature = new Signature(credentials)
 }
 
 Database.prototype.request = function(target, data, cb) {
   !function retry(database, i) {
     var req = new Request(database.host, target, data || {})
 
-    database.strategy.sign(req, function(err) {
-      if (err) return cb(err)
-
-      req.send(function(err, data) {
-        if (err) {
-          if (i < Request.prototype.maxRetries && (
-            err.statusCode == 500 ||
-            err.statusCode == 503 ||
-            err.name.slice(-38) == "ProvisionedThroughputExceededException"
-          )) {
-            setTimeout(retry, 50 << i, database, i + 1)
-          }
-
-          else cb(err)
+    database.signature.sign(req)
+    
+    req.send(function(err, data) {
+      if (err) {
+        if (i < Request.prototype.maxRetries && (
+          err.statusCode == 500 ||
+          err.statusCode == 503 ||
+          err.name.slice(-38) == "ProvisionedThroughputExceededException"
+        )) {
+          setTimeout(retry, 50 << i, database, i + 1)
         }
 
-        else cb(null, data)
-      })
+        else cb(err)
+      }
+
+      else cb(null, data)
     })
   }(this, 0)
 }
 
 function Request(host, target, data) {
-  var headers = this.headers = new RequestHeaders
+  var headers = this.headers = {}
 
   this.body = JSON.stringify(data)
 
@@ -49,23 +44,15 @@ function Request(host, target, data) {
   headers["X-Amz-Target"] = Request.prototype.target + target
   headers["Host"] = this.host = host
   headers["Content-Length"] = Buffer.byteLength(this.body)
+  headers["Content-Type"] = Request.prototype.contentType
 }
 
-Request.prototype.method     = "POST"
-Request.prototype.pathname   = "/"
-Request.prototype.target     = "DynamoDB_20111205."
-Request.prototype.service    = "dynamodb"
-Request.prototype.data       = {}
-Request.prototype.maxRetries = 10
-
-Request.prototype.toString = function() {
-  return this.method +
-    "\n" + this.pathname +
-    "\n" +
-    "\n" + this.headers +
-    "\n" +
-    "\n" + this.body
-}
+Request.prototype.method      = "POST"
+Request.prototype.pathname    = "/"
+Request.prototype.target      = "DynamoDB_20111205."
+Request.prototype.service     = "dynamodb"
+Request.prototype.maxRetries  = 10
+Request.prototype.contentType = "application/x-amz-json-1.0"
 
 Request.prototype.send = function(cb) {
   var request = http.request(this, function(res) {
@@ -94,155 +81,6 @@ Request.prototype.send = function(cb) {
   request.end()
 }
 
-function RequestHeaders() {
-  this["Content-Type"] = RequestHeaders.prototype["Content-Type"]
-}
-
-RequestHeaders.prototype["Content-Type"] = "application/x-amz-json-1.0"
-
-RequestHeaders.prototype.toString = function() {
-  return "host:"                 + this["Host"] +
-       "\nx-amz-date:"           + this["X-Amz-Date"] +
-       "\nx-amz-security-token:" + this["X-Amz-Security-Token"] +
-       "\nx-amz-target:"         + this["X-Amz-Target"]
-}
-
-function Session(attrs) {
-  this.sessionCredentials = new Credentials(attrs || {})
-  this.tokenCredentials = null
-  this.listeners = []
-}
-
-Session.prototype.duration         = 60 * 60 * 1000
-Session.prototype.refreshPadding   = 60 * 1000 //refresh 1 minute ahead of time
-Session.prototype.consumedCapacity = 0
-
-Session.prototype.sign = function(request, date, cb) {
-  if (!cb) {
-    cb = date
-    date = new Date
-  }
-
-  this.fetch(function(err, session) {
-    if (err) return cb(err)
-
-    var hash = crypto.createHash("sha256")
-      , payload
-
-    request.headers["X-Amz-Security-Token"] = session.token
-    request.headers["X-Amz-Date"] = request.headers["Date"] = date.toUTCString()
-
-    payload = new Buffer(request.toString(), "utf8")
-    hash = hash.update(payload).digest()
-
-    request.headers["X-Amzn-Authorization"] = "AWS3 " + [
-      "AWSAccessKeyId=" + session.tokenCredentials.accessKeyId,
-      "Algorithm=HmacSHA256",
-      "SignedHeaders=host;x-amz-date;x-amz-security-token;x-amz-target",
-      "Signature=" + session.tokenCredentials.sign(hash)
-    ]
-
-    cb(null, request)
-  })
-}
-
-Session.prototype.fetch = function(cb) {
-  if ((this.expiration - this.refreshPadding) > new Date) return cb(null, this)
-
-  this.listeners.push(cb) > 1 || this.refresh()
-}
-
-Session.prototype.refresh = function() {
-  var req = new SessionRequest
-
-  req.query.DurationSeconds = 0 | this.duration / 1000
-  req.query.AWSAccessKeyId = this.sessionCredentials.accessKeyId
-  req.query.Signature = this.sessionCredentials.sign(req.toString(), "sha256", "base64")
-
-  req.send(function(err, data) {
-    var listeners = this.listeners.splice(0)
-
-    if (!err) {
-      this.expiration = new Date(data.expiration)
-      this.tokenCredentials = new Credentials(data)
-      this.token = data.sessionToken
-    }
-
-    listeners.forEach(function(cb) {
-      cb(err, err ? null : this)
-    }, this)
-  }.bind(this))
-}
-
-function SessionRequest() {
-  this.query = new SessionQuery
-}
-
-SessionRequest.prototype.method   = "GET"
-SessionRequest.prototype.host     = "sts.amazonaws.com"
-SessionRequest.prototype.pathname = "/"
-
-SessionRequest.prototype.toString = function() {
-  return   this.method +
-    "\n" + this.host   +
-    "\n" + this.pathname +
-    "\n" + this.query.toString().slice(1)
-}
-
-SessionRequest.prototype.send = function(cb) {
-  var signature = encodeURIComponent(this.query.Signature)
-    , query = this.query + "&Signature=" + signature
-    , path = Request.prototype.pathname + query
-    , options = { host: this.host, path: path }
-
-  https.get(options, function(res) {
-    var xml = ""
-
-    res.on("data", function(chunk){ xml += chunk })
-    res.on("end", function() {
-      var response = new SessionResponse(xml)
-
-      if (res.statusCode == 200) cb(null, response)
-
-      else cb(new Error(
-        response.type + "(" + response.code + ")\n\n" +
-        response.message
-      ))
-    })
-  })
-}
-
-function SessionQuery() {
-  this.Timestamp = (new Date).toISOString().slice(0, 19) + "Z"
-}
-
-SessionQuery.prototype.Action           = "GetSessionToken"
-SessionQuery.prototype.SignatureMethod  = "HmacSHA256"
-SessionQuery.prototype.SignatureVersion = "2"
-SessionQuery.prototype.Version          = "2011-06-15"
-
-SessionQuery.prototype.toString = function() {
-  return (
-    "?AWSAccessKeyId="   + this.AWSAccessKeyId +
-    "&Action="           + this.Action +
-    "&DurationSeconds="  + this.DurationSeconds +
-    "&SignatureMethod="  + this.SignatureMethod +
-    "&SignatureVersion=" + this.SignatureVersion +
-    "&Timestamp="        + encodeURIComponent(this.Timestamp) +
-    "&Version="          + this.Version
-  )
-}
-
-function SessionResponse(xml) {
-  var tag, key, regexp = /<(\w+)>(.*)</g
-
-  while (tag = regexp.exec(xml)) {
-    key = tag[1]
-    key = key.charAt(0).toLowerCase() + key.slice(1)
-    this[key] = tag[2]
-  }
-}
-
 function Credentials(attrs) {
   var env = process.env
 
@@ -256,32 +94,18 @@ function Credentials(attrs) {
   if (!this.accessKeyId) {
     throw new Error("No access key id available.")
   }
-
-  this.sign = function(data) {
-    return crypto
-      .createHmac("sha256", this.secretAccessKey)
-      .update(data)
-      .digest("base64")
-  }
 }
 
 function Signature(attrs) {
   this.credentials = new Credentials(attrs || {})
 }
 
-Signature.prototype.sign = function(request, date, cb) {
-  if (!cb) {
-    cb = date
-    date = new Date
-  }
-
-  var datetime = date.toISOString().replace(/[:\-]|\.\d{3}/g, "")
+Signature.prototype.sign = function(request, date) {
+  var datetime = (date || new Date).toISOString().replace(/[:\-]|\.\d{3}/g, "")
     , authRequest = new SignatureRequest(this.credentials, request, datetime)
 
   request.headers["Date"] = request.headers["X-Amz-Date"] = datetime
   request.headers["Authorization"] = authRequest.createHeader()
-
-  process.nextTick(cb.bind(null, null, request))
 }
 
 // credentials expects: { accessKeyId, secretAccessKey }
@@ -364,15 +188,10 @@ SignatureRequest.prototype.sha256Digest = function(key, data, encoding) {
 
 dynamo.Database         = Database
 dynamo.Request          = Request
-dynamo.RequestHeaders   = RequestHeaders
-dynamo.Session          = Session
-dynamo.SessionRequest   = SessionRequest
-dynamo.SessionQuery     = SessionQuery
-dynamo.SessionResponse  = SessionResponse
 dynamo.Credentials      = Credentials
 dynamo.Signature        = Signature
 dynamo.SignatureRequest = SignatureRequest
 
-dynamo.createClient = function(host, strategy) {
-  return new Database(host, strategy)
+dynamo.createClient = function(host, credentials) {
+  return new Database(host, credentials)
 }
